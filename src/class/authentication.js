5161a1b9f3df477d8469a68673b2369206a16ba8
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-01 18:16:55 +0200

    feat(class/authentication.js) authentication in progress

---
+++ src/class/authentication.js
@@ -0,0 +1,11 @@
+export default class Authentication {
+
+  _session() {
+    // TODO: Authenticate user with session
+  }
+
+  _basicAuth() {
+    // TODO: Authenticate user with basic auth
+  }
+
+}
--- src/class/base.js
+++ src/class/base.js
@@ -25,4 +25,8 @@ export default class Base extends Configuration {
     return `${querystring.stringify(output)}`
   }
 
+  authenticate() {
+    // TODO: Authenticate with session
+  }
+
 }
