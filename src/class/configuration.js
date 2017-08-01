09068fb84a291fdd101498b88e40b4186b83214c
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-01 15:58:52 +0200

    feat(class/configuration.js) class created

--- src/class/configuration.js
+++ src/class/configuration.js
@@ -1,7 +1,51 @@
 export default class Configuration {
 
+  /**
+   * @description Auto-hydrate object from configuration
+   * @param config
+   */
   constructor(config) {
     this._configure(config, this._itemsToHydrate())
+  }
+
+  get environment() {
+    return this._environment
+  }
+
+  set environment(value) {
+    this._environment = value
+  }
+
+  get output() {
+    return this._output
+  }
+
+  set output(value) {
+    this._output = value
+  }
+
+  get origin() {
+    return this._origin
+  }
+
+  set origin(value) {
+    this._origin = value
+  }
+
+  get authentication() {
+    return this._authentication
+  }
+
+  set authentication(value) {
+    this._authentication = value
+  }
+
+  get credentials() {
+    return this._credentials
+  }
+
+  set credentials(value) {
+    this._credentials = value
   }
 
   /**
@@ -22,7 +66,7 @@ export default class Configuration {
    * @private
    */
   _itemsToHydrate() {
-    return ['environment', 'output', 'origin', 'authentication', 'credentials']
+    return ['_environment', '_output', '_origin', '_authentication', '_credentials']
   }
 
 }
