ec2bfa12339d15a685b0e7f03800eea96c221f73
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-01 17:06:21 +0200

    feat(class/exceptions/index.js) entry point created to collect and export easly

---
+++ src/class/exceptions/index.js
@@ -0,0 +1,15 @@
+import AppointmentNotFound from './appointment/not_found'
+import CalendarNotFound from './calendar/not_found'
+import ConfigurationWrongEnvironment from './configuration/wrong_environment'
+import FicheNotFound from './fiche/not_found'
+import GroupNotFound from './group/not_found'
+import InterventionNotFound from './intervention/not_found'
+
+export {
+  AppointmentNotFound,
+  CalendarNotFound,
+  ConfigurationWrongEnvironment,
+  FicheNotFound,
+  GroupNotFound,
+  InterventionNotFound
+}
