6d4896a5ba12eba1b7fa33b9f6ae9905d7a0d0e2
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-01 15:48:42 +0200

    feat(class/base.js) hydrate configuration from base class

--- src/class/base.js
+++ src/class/base.js
@@ -2,6 +2,15 @@ import querystring from 'querystring'
 
 export default class Base {
 
+  constructor(config) {
+    this._configure(config, this._itemsToHydrate())
+  }
+
+  /**
+   * @description Generator conditions string from object
+   * @param filters
+   * @return {string}
+   */
   filter(filters) {
     const output = Object.keys(filters)
       .map((key) => [key, filters[key]])
@@ -13,6 +22,27 @@ export default class Base {
         return tmp
       }, {})
     return `${querystring.stringify(output)}`
+  }
+
+  /**
+   * @description Hydrate object from configuration settings
+   * @param obj
+   * @param attributes
+   */
+  _configure(obj, attributes) {
+    if (!obj) return
+    for (let item of attributes) {
+      this[item] = (obj[item]) ? obj[item] : ''
+    }
+  }
+
+  /**
+   * @description Return array with items names to hydrate
+   * @return {[string,string,string,string,string]}
+   * @private
+   */
+  _itemsToHydrate() {
+    return ['environment', 'output', 'origin', 'authentication', 'credentials']
   }
 
 }
